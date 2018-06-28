4242c02e2e61780666148909d3f683fbdcbc97a6
github username <github email address>
2018-06-28 13:04:01 -0400

    first commit

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,31 @@
-class CrewMember {}
+class CrewMember {
+
+  constructor(position,currentShip) {
+    this.position = position;
+    if (currentShip) {
+      this.currentShip = currentShip;
+    } else {
+      this.currentShip = 'Looking for a Rig'
+    }    
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect'; 
+    }
+  }
+
+  setsInvisibility() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect'; 
+    }
+  }
+
+  engageWarpDrive() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect'; 
+    }
+  }
+
+
+}
