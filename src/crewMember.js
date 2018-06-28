2c2da7bbdbe45543eb7905a993d8b0cd0c418002
Ledio <L.Bode515@gmail.com>
2018-06-28 14:32:02 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,46 @@
-class CrewMember {}
+let crewMemberId = 0;
+let stores = {
+  crewMembers: [],
+  ships: []
+}
+
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.id = ++crewMemberId;
+    this.shipID;
+
+    stores.crewMembers.push(this);
+  }
+    currentShip() {
+      let that = this;
+      if (this.shipID){ this.currentShip = stores.ships.find(function(ship){ return ship.id === that.shipID})
+    } else { this.currentShip =  'Looking for a Rig'}
+
+    }
+    chargePhasers() {
+      if (this.shipID && this.position === 'gunner') {
+        this.currentShip.warpDrive = true;
+
+      } else {
+        return  "had no effect"
+      }
+
+}
+    engageWarpDrive() {
+      if (this.shipID && this.position ==='pilot') {
+        this.currentShip.warpDrive = 'engaged';
+      } else {
+        return  "had no effect"
+      }
+
+}
+    setsInvisibility() {
+      if (this.shipID && this.position ==='defender' ) {
+           this.currentShip.cloaked = true;
+      } else {
+        return  "had no effect"
+      }
+
+}
+}
