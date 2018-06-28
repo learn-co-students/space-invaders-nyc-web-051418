c37c02b6140742e49c83ed5dca7df0bea0dc4c2a
squamuglia <maxsmouha@gmail.com>
2018-06-28 09:36:39 -0400

    crew member class instantiated

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,27 @@
-class CrewMember {}
+class CrewMember {
+    constructor (position, name, currentShip = 'Looking for a Rig') {
+      this.name = name
+      this.position = position
+      this.currentShip = currentShip
+    }
+
+    engageWarpDrive(){
+      if (this.currentShip === 'Looking for a Rig') {
+          return 'had no effect'
+      }
+    }
+
+    setsInvisibility() {
+      if (this.currentShip === 'Looking for a Rig') {
+          return 'had no effect'
+      }
+
+    }
+
+    chargePhasers() {
+      if (this.currentShip === 'Looking for a Rig') {
+          return 'had no effect'
+      }
+
+    }
+}
